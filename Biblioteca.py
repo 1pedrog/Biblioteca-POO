bba481510b5e575285926987682132b44298115a
Thiago_mota _program <thiagomotamachado@gmail.com>
2025-06-30 14:52:30 -0300

    AddOneBookToLibary

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,5 +1,22 @@
 
 
+from dataclasses import dataclass
 
+@dataclass
+class Book:
+    title: str
+    author: str
+    year: int
+    genre: str
+    pages: int
 
-print("Hello world")
+book1 = Book(
+    title="1984",
+    author="George Orwell",
+    year=1949,
+    genre="Dystopian",
+    pages=328
+)
+
+print(book1)
+print(f"{book1.title} foi escrito por {book1.author} em {book1.year}.")
