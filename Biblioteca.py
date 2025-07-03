aa045a15f6fbcc7b1f37e5c4492f5e75e6f975f5
Thiago_mota _program <thiagomotamachado@gmail.com>
2025-07-03 18:44:58 -0300

    IntegratingBookSearchAndMain

--- Biblioteca.py
+++ Biblioteca.py
@@ -4,43 +4,13 @@
 ##TODO: regra - não emprestar livros indisponiveis
 
 from organizar_biblioteca import escolher_biblioteca
+from BookSearch import start_book_search
 
-#Criação da classe Book
-class Book:
-    def __init__(self, title, author, year, genre, pages, isRented):
-        self.title = title
-        self.author = author
-        self.year = year
-        self.genre = genre
-        self.pages = pages
-        self.isRented = isRented
+def main():
+    biblioteca = escolher_biblioteca()
+    print(f"\nVocê escolheu a biblioteca: {biblioteca.name}")
 
-#Criação da classe Cliente
-class Cliente:
-    def __init__(self, nome, livrosAlugados, idade, isMale):
-        self.nome = nome
-        self.livrosAlugados = livrosAlugados  # Lista de objetos Book
-        self.idade = idade
-        self.isMale = isMale
+    start_book_search()
 
-book1 = Book( ##aqui foi criado o livro
-    title="1984",
-    author="George Orwell",
-    year=1949,
-    genre="Dystopian",
-    pages=328,
-    isRented=False
-)
-client1 = Cliente( ##aqui foi criado o cliente
-    nome="João Silva",
-    livrosAlugados=[],
-    idade=30,
-    isMale=True
-)
-
-print(f"Gênero: {book1.genre}, Páginas: {book1.pages}, Disponível: {not book1.isRented}")
-print(f"Cliente: {client1.nome}, Idade: {client1.idade}, Gênero: {'Masculino' if client1.isMale else 'Feminino'}")
-#Puxa a função para escolher a biblioteca
-biblioteca_escolhida = escolher_biblioteca()
-#if biblioteca_escolhida.idBiblioteca: #Puxando a função de login
-#   usuario = login_user()
+if __name__ == "__main__":
+    main()
